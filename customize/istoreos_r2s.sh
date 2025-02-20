0fd4361c8284e37c9c40f9fca8a8c3b7936999b6
DSKing377 <lyqf0377@gmail.com>
2025-02-20 11:00:21 +0800

    Update istoreos_r2s.sh

--- customize/istoreos_r2s.sh
+++ customize/istoreos_r2s.sh
@@ -92,7 +92,7 @@ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-
 
 ##########################################其他设置##########################################
 # 修改默认登录地址
-# sed -i 's/192.168.1.1/192.168.2.1/g' ./package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.1/g' ./package/base-files/files/bin/config_generate
 
 # 修改默认登录密码
 sed -i 's/$1$5mjCdAB1$Uk1sNbwoqfHxUmzRIeuZK1//g' ./package/base-files/files/etc/shadow
